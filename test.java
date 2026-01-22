f0c9c8b9274e2ab3f90a0665b72aa7cdb1d50405
livak <root@SnedvinsDator.localdomain>
2026-01-22 22:07:03 +0100

    test: Added two unit tests for Lic11 using JUnit 4 in VScode. Not 100% sure if they are done correctly though.

--- test.java
+++ test.java
@@ -1,7 +1,41 @@
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
 public class test {
     
 
+@Before
+public void setup() {
+     // Initialize PARAMETERS before each test.
+    Main.PARAMETERS = new Main.Parameters();
+}
 
+@Test
+public void lic11_false_whenNumPointsLessThan3() {
+    Main.NUMPOINTS = 2;
+    Main.X = new double[] { 1.0, 0.0 };
+    Main.Y = new double[] { 0.0, 0.0 };
+    Main.PARAMETERS.G_PTS = 1;
+
+    boolean[] cmv = Cmv.computeCMV();
+    assertFalse(cmv[11]);
+}
+
+@Test
+public void lic11_false_whenGPTsOutOfRange() {
+    Main.NUMPOINTS = 4;
+    Main.X = new double[] {3, 2, 1, 0 };
+    Main.Y = new double[] {0, 0, 0, 0 };
+
+    Main.PARAMETERS.G_PTS = 0; // Invalid, must be >= 1
+
+    boolean[] cmv = Cmv.computeCMV();
+    assertFalse(cmv[11]);
+}
 
 
 
