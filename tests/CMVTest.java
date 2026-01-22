bc99235e36878097b3955794a4fa369e5d3adbed
amanda-zakir <amandazakir@gmail.com>
2026-01-22 12:27:16 +0100

    create CMVTest.java
    set up test framwork for lic0

---
+++ tests/CMVTest.java
@@ -0,0 +1,19 @@
+import com.Cmv;
+import com.Main;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class CmvTest {
+    @Test
+    public void testLic0() {
+        Parameters lic = new Parameters();
+        Cmv cmv = new Cmv();
+
+        assertTrue(cmv[0]);
+        
+
+
+    }
+}
+
