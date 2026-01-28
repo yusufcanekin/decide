274b9df9f1cd428311b456662c4be296363c7716
livak <root@SnedvinsDator.localdomain>
2026-01-28 21:35:27 +0100

    feat + bug: Removed bad tests for decide integration and added new ones that test the entire solution more thoroughly. Also altered FUV  function so that it ignores diagonal elements, which it didn't do previously. This caused the tests to give false when they shouldn't have.

--- src/test/java/DecideTest.java
+++ src/test/java/DecideTest.java
@@ -12,7 +12,7 @@ public class DecideTest {
     private Main.Connector[][] makeLCM(Main.Connector fill) {
         Main.Connector[][] lcm = new Main.Connector[15][15];
         for (int i = 0; i < 15; i++) {
-            for (int j = 0; j > 15; j++) {
+            for (int j = 0; j < 15; j++) {
                 lcm[i][j] = fill;
             }
         }
@@ -21,81 +21,172 @@ public class DecideTest {
 
     /**
      * Asserts true for:
-     * - All PUV values are false
-     * - Therefore all FUV values are true
-     * - Therefore final LAUNCH decision is true
+     * - All PUV values are false --> all FUV values are true.
+     * - Therefore final LAUNCH decision is true.
      */
     @Test
-    public void decide_true_whenAllPUVFalse() {
-        // Minimal points (CMV content doesn't matter because PUV disables all LICs)
-        Main.NUMPOINTS = 2;
-        Main.X = new double[] { 0.0, 0.0 };
-        Main.Y = new double[] { 0.0, 0.0 };
+    public void decide_true_whenExpectedToBeTrueAndPUVAllFalse() {
+        Main.NUMPOINTS = 5;
         Main.PARAMETERS = new Main.Parameters();
 
-        // LCM can be anything; NOTUSED makes PUM trivially true
-        Main.LCM = makeLCM(Main.Connector.NOTUSED);
+        Main.PARAMETERS.LENGTH1 = 0.5;
+        Main.PARAMETERS.RADIUS1 = 0.5;
+        Main.PARAMETERS.EPSILON = 1.0;
+        Main.PARAMETERS.AREA1 = 0.5;
 
-        // All PUV false => all FUV true
+        Main.PARAMETERS.Q_PTS = 4;
+        Main.PARAMETERS.QUADS = 1;
+
+        Main.PARAMETERS.DIST = 0.5;
+        Main.PARAMETERS.N_PTS = 3;
+
+        Main.PARAMETERS.K_PTS = 1;
+        Main.PARAMETERS.A_PTS = 1;
+        Main.PARAMETERS.B_PTS = 1;
+        Main.PARAMETERS.C_PTS = 1;
+        Main.PARAMETERS.D_PTS = 1;
+        Main.PARAMETERS.E_PTS = 1;
+        Main.PARAMETERS.F_PTS = 1;
+        Main.PARAMETERS.G_PTS = 1;
+
+        Main.PARAMETERS.LENGTH2 = 3.0;
+        Main.PARAMETERS.RADIUS2 = 5.0;
+        Main.PARAMETERS.AREA2 = 5.0;
+
+        Main.X = new double[] { 0.0, 1.0, 2.0, 4.0, -1.0 };
+        Main.Y = new double[] { 0.0, 1.0, 2.0, 0.0, 0.0 };
+
+        Main.LCM = makeLCM(Main.Connector.ORR);
         Main.PUV = new boolean[15]; // default false
 
         Main.decide();
+        assertTrue(Main.LAUNCH);
+    }
+
+    @Test
+    public void decide_true_whenExpectedToBeTrueAndPUVAllTrue() {
+        Main.NUMPOINTS = 5;
+        Main.PARAMETERS = new Main.Parameters();
+
+        Main.PARAMETERS.LENGTH1 = 0.5;
+        Main.PARAMETERS.RADIUS1 = 0.5;
+        Main.PARAMETERS.EPSILON = 1.0;
+        Main.PARAMETERS.AREA1 = 0.5;
+
+        Main.PARAMETERS.Q_PTS = 4;
+        Main.PARAMETERS.QUADS = 1;
+
+        Main.PARAMETERS.DIST = 0.5;
+        Main.PARAMETERS.N_PTS = 3;
+
+        Main.PARAMETERS.K_PTS = 1;
+        Main.PARAMETERS.A_PTS = 1;
+        Main.PARAMETERS.B_PTS = 1;
+        Main.PARAMETERS.C_PTS = 1;
+        Main.PARAMETERS.D_PTS = 1;
+        Main.PARAMETERS.E_PTS = 1;
+        Main.PARAMETERS.F_PTS = 1;
+        Main.PARAMETERS.G_PTS = 1;
+
+        Main.PARAMETERS.LENGTH2 = 3.0;
+        Main.PARAMETERS.RADIUS2 = 5.0;
+        Main.PARAMETERS.AREA2 = 5.0;
+
+        Main.X = new double[] { 0.0, 1.0, 2.0, 4.0, -1.0 };
+        Main.Y = new double[] { 0.0, 1.0, 2.0, 0.0, 0.0 };
+        // CMV[i] true for all i
+        // When LCM is ORR --> PUM[i][j] true for all i,j
+        // When PUV[i] true for alla i -->
+        // must check that all PUM[i][j] true for FUV[i] to be true
+
+        Main.LCM = makeLCM(Main.Connector.ORR);
+        Main.PUV = new boolean[15]; // default false
+        for (int i = 0; i < 15; i++) {
+            Main.PUV[i] = true;
+        }
+
+        System.out.println("LCM[0][1] = " + Main.LCM[0][1]);
+        System.out.println("LCM[0][0] = " + Main.LCM[0][0]);
+        System.out.println("LCM[2][3] = " + Main.LCM[2][3]);
+        System.out.println("PUV[0] = " + Main.PUV[0]);
+
+        Main.decide();
+        // Check how many CMVs are actually true
+        int trueCount = 0;
+        for (int i = 0; i < 15; i++) {
+            if (Main.CMV[i])
+                trueCount++;
+        }
+        System.out.println("CMV true count = " + trueCount);
+
+        printPUM(Main.PUM);
+
+        // Show which FUV entries fail
+        for (int i = 0; i < 15; i++) {
+            if (!Main.FUV[i]) {
+                System.out.println("FUV[" + i + "] is false, CMV[" + i + "]=" + Main.CMV[i]);
+            }
+        }
 
         assertTrue(Main.LAUNCH);
     }
 
-    /**
-     * Asserts false for:
-     * - At least one LIC is enabled in PUV
-     * - decide() runs CMV -> PUM ->FUV
-     * - LAUNCH decisiion is false
-     */
     @Test
-    public void decide_false_whenAtLeastOnePUVTrue() {
-        // NUMPOINTS + points
-        Main.NUMPOINTS = 3;
-        Main.X = new double[] {0.0, 1.0, 2.0};
-        Main.Y = new double[] {0.0, 0.0, 0.0};
-
+    public void decide_falseWhenExpectedToBeFalse() {
+        Main.NUMPOINTS = 2;
         Main.PARAMETERS = new Main.Parameters();
 
-        // LCM provided
-        Main.LCM = makeLCM(Main.Connector.NOTUSED);
+        Main.PARAMETERS.LENGTH1 = 1000.0;
+        Main.PARAMETERS.RADIUS1 = 100.0;
+        Main.PARAMETERS.EPSILON = Main.PARAMETERS.PI;
+        Main.PARAMETERS.AREA1 = 1000.0;
 
-        // Enable one LIC (index choice irrelevant for this test)
+        Main.PARAMETERS.Q_PTS = 2;
+        Main.PARAMETERS.QUADS = 4;
+
+        Main.PARAMETERS.DIST = 0.00001;
+        Main.PARAMETERS.N_PTS = 2;
+
+        Main.PARAMETERS.K_PTS = 1;
+        Main.PARAMETERS.A_PTS = 1;
+        Main.PARAMETERS.B_PTS = 1;
+        Main.PARAMETERS.C_PTS = 1;
+        Main.PARAMETERS.D_PTS = 1;
+        Main.PARAMETERS.E_PTS = 1;
+        Main.PARAMETERS.F_PTS = 1;
+        Main.PARAMETERS.G_PTS = 1;
+
+        Main.PARAMETERS.LENGTH2 = 0.00001;
+        Main.PARAMETERS.RADIUS2 = 0.001;
+        Main.PARAMETERS.AREA2 = 0.00001;
+
+        Main.X = new double[] { 0.0, 1.0, 2.0, 3.0, 4.0 };
+        Main.Y = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0 };
+
+        Main.LCM = makeLCM(Main.Connector.ANDD);
         Main.PUV = new boolean[15];
-        Main.PUV[0] = true;
-
-        Main.decide();
-        
-        assertFalse(Main.LAUNCH);
+        for (int i = 0; i < 15; i++) {
+            Main.PUV[i] = true;
         }
 
-    /**
-     * Asserts false for:
-     * - At least one FUV value is false
-     * - Therefore final LAUNCH decision is false
-     */
-    @Test
-    public void decide_false_whenAnyFUVFalse() {
-        // Points such that CMV[0] becomes false
-        Main.NUMPOINTS = 2;
-        Main.X = new double[] {0.0, 0.0};
-        Main.Y = new double[] {0.0, 0.0};
-        Main.PARAMETERS = new Main.Parameters();
-        Main.PARAMETERS.LENGTH1 = 1.0;          // Distance is 0 => LIC0 false
-
-        // Make LCM ANDD so row 0 depends on CMV[0] && CMV[j] (false)
-        Main.LCM = makeLCM(Main.Connector.ANDD);
-
-        // Only LIC0 matters
-        Main.PUV = new boolean[15];
-        Main.PUV[0] = true;
-
         Main.decide();
-
         assertFalse(Main.LAUNCH);
     }
 
+    private static void printPUM(boolean[][] pum) {
+        System.out.println("PUM matrix:");
+        System.out.print("      ");
+        for (int j = 0; j < 15; j++) {
+            System.out.printf("%4d", j);
+        }
+        System.out.println();
 
+        for (int i = 0; i < 15; i++) {
+            System.out.printf("%4d: ", i);
+            for (int j = 0; j < 15; j++) {
+                System.out.printf("%4s", pum[i][j] ? "T" : "F");
+            }
+            System.out.println();
+        }
+    }
 }
